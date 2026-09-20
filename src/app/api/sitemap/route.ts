ce408a44a07beb92932979c013ff9094fa985c64
aminurrahman4078 <aminurrahman9793@gmail.com>
2026-09-20 21:28:31 +0600

    hgujjkhk

--- src/app/api/sitemap/route.ts
+++ src/app/api/sitemap/route.ts
@@ -3,6 +3,8 @@ import { db } from "../../../lib/db";
 import { env } from "../../../lib/env";
 import { generateSitemap } from "../../../utils/sitemap";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   const projects = env.DATABASE_URL ? await db.project.findMany({ where: { published: true }, select: { slug: true } }) : [];
   const pages = ["/", ...projects.map((project) => `/projects/${project.slug}`)];
